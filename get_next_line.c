31da30d7de4405f81f5bea7530c5d0ac5792d914
Wolf Hendrikse <whendrik@z1t2c2.42bangkok.com>
2023-05-13 15:15:17 +0700

    Preclean up ,refix norm and submit

--- get_next_line.c
+++ get_next_line.c
@@ -28,77 +28,132 @@ char	*ft_free_join(char *res, char *buffer)
 	return (totalstr);
 }
 
-char *conditionator(char *res, char **buffer, int bytes_read)
+// char *conditionator(char **res, char **buffer, int b_r)
+// {
+//     if (b_r < 0)
+//     {
+//         free(*buffer);
+//         free(*res);
+//         return (NULL);
+//     }
+
+//     if (b_r > 0)
+//     {
+// 		if (ft_strchr(*buffer, '\n'))
+//         {    
+//             *res = ft_free_join(*res, *buffer);
+//             *buffer = NULL;
+//             return (*res);
+//         }
+//         else
+//         {
+//             *res = ft_free_join(*res, *buffer);
+//             *buffer = NULL;
+//         }
+//     }
+//     return (*res);
+// 	// return (NULL);
+// }
+
+char *read_file_helper(char *res, char *buffer, int b_r)
 {
-    if (bytes_read < 0)
-    {
-        free(*buffer);
-        free(res);
-        return (NULL);
-    }
-
-    if (bytes_read > 0)
-    {
-		if (ft_strchr(*buffer, '\n'))
-        {    
-            res = ft_free_join(res, *buffer);
-            *buffer = NULL;
-            return (res);
-        }
-        else
-        {
-            res = ft_free_join(res, *buffer);
-            *buffer = NULL;
-        }
-    }
-    return (res);
+    if (b_r > 0)
+	{
+		if (ft_strchr(buffer, '\n'))
+		{
+			res = ft_free_join(res, buffer);
+			buffer = NULL;
+			return res;
+		}
+		else
+		{
+			res = ft_free_join(res, buffer);
+			buffer = NULL;
+			return res;
+		}
+	}
+	else if (buffer && buffer[0] != '\n' && buffer[b_r] != '\n')
+	{
+		free(buffer);
+		return res;
+	}
+	return res;
 }
-
 char	*read_file(int fd, char *res)
 {
-	int		bytes_read;
+	int		b_r;
 	char	*buffer;
 
-	buffer = NULL;
 	if (!res || res[0] == '\0')
 	{
-		res = malloc(1);
-		res[0] = '\0';
+		res = specific_calloc(1, 1);
 	}
-	bytes_read = 1;
-	while (bytes_read > 0)
+	b_r = 1;
+	while (b_r > 0)
 	{
 		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		buffer[bytes_read] = 0;
-		// res = conditionator(res, &buffer, bytes_read);
+		// printf("readcount: %d\n", b_r);
+		b_r = read(fd, buffer, BUFFER_SIZE);
+		buffer[b_r] = 0;
 		// if (res == NULL)
 		// 	return NULL;
-		if (bytes_read < 0)
+		if (b_r < 0)
 		{
 			free(buffer);
 			free(res);
 			return (NULL);
 		}
-		if (bytes_read > 0)
-		{
-			if (ft_strchr(buffer, '\n'))
-			{
-				res = ft_free_join(res, buffer);
-				buffer = NULL;
-				break ;
-			}
-			else
-			{
-				res = ft_free_join(res, buffer);
-				buffer = NULL;
-			}
-		}
+		res = read_file_helper(res, buffer, b_r);
+		if (ft_strchr(res, '\n'))
+			return res;
 	}
-	if ((buffer != NULL && buffer[0] != '\n' && buffer[bytes_read] != '\n'))
-		free(buffer);
 	return (res);
 }
+// char	*read_file(int fd, char *res)
+// {
+// 	int		b_r;
+// 	char	*buffer;
+
+// 	// buffer = NULL;
+// 	if (!res || res[0] == '\0')
+// 	{
+// 		res = malloc(1);
+// 		res[0] = '\0';
+// 	}
+// 	b_r = 1;
+// 	while (b_r > 0)
+// 	{
+// 		buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
+// 		b_r = read(fd, buffer, BUFFER_SIZE);
+// 		buffer[b_r] = 0;
+// 		// conditionator(&res, &buffer, b_r);
+// 		if (res == NULL)
+// 			return NULL;
+// 		if (b_r < 0)
+// 		{
+// 			free(buffer);
+// 			free(res);
+// 			return (NULL);
+// 		}
+// 		if (b_r > 0)
+// 		{
+// 			if (ft_strchr(buffer, '\n'))
+// 			{
+// 				res = ft_free_join(res, buffer);
+// 				buffer = NULL;
+// 				break ;
+// 			}
+// 			else
+// 			{
+// 				res = ft_free_join(res, buffer);
+// 				buffer = NULL;
+// 			}
+// 		}
+// 	}
+// 	if ((buffer && buffer[0] != '\n' && buffer[b_r] != '\n'))
+// 		free(buffer);
+// 	return (res);
+// }
 
 char	*ft_line(char *buffer)
 {
@@ -107,16 +162,17 @@ char	*ft_line(char *buffer)
 	int		len;
 	// int		blen;
 
-	printf("see%s", buffer);
+	// // printf("see%s", buffer);
 	if (!buffer)
 		return (NULL);
 	len = ft_strlen(buffer, '\n');
 	// blen = ft_strlen(buffer, '\0');
-	// printf(">%d = %d\n", len, blen);
+	// // printf(">%d = %d\n", len, blen);
 	res = (char *)malloc((len + 1) * sizeof(char));
 	if (!res)
 		return (NULL);
-	// res[len] = 0;
+	res[len] = 0;
+	// ft_memcpy(res, buffer, len + 1);
 	i = 0;
 	while (buffer[i] != '\n' && buffer[i] != '\0')
 	{
@@ -128,13 +184,14 @@ char	*ft_line(char *buffer)
 	// 	res[i] = '\0';
 	// } else
 
-	// if (buffer[i] == '\n')
-	// {
-	// 	res[i] = '\n';
-	// }
-	res[i] = '\n';
-	res[len] = 0;
-	printf("res%s", res);
+	if (buffer[i] == '\n')
+	{
+		res[i] = '\n';
+		i++;
+	}
+	// res[i] = '\n';
+	res[i] = 0;
+	// printf("res end of ft_line: %s", res);
 	return (res);
 }
 // char	*ft_line(char *buffer)
@@ -148,7 +205,7 @@ char	*ft_line(char *buffer)
 // 		return (NULL);
 // 	len = ft_strlen(buffer, '\n');
 // 	// blen = ft_strlen(buffer, '\0');
-// 	// printf(">%d = %d\n", len, blen);
+// 	// // printf(">%d = %d\n", len, blen);
 // 	res = (char *)malloc((len + 1) * sizeof(char));
 // 	if (!res)
 // 		return (NULL);
@@ -184,9 +241,10 @@ char	*next_line(char *buffer)
 	}
 	line = malloc((ft_strlen(buffer, '\0') - i + 2) * sizeof(char));
 	j = 0;
-	printf("buff in nextline:%s", buffer);
 	while (buffer[i])
 		line[j++] = buffer[i++];
+	// ft_memcpy(line, buffer, BUFFER_SIZE + 1);
+	// printf("line after memcpy in nextline: %s", buffer);
 	line[j] = 0;
 	free(buffer);
 	return (line);
@@ -208,9 +266,10 @@ char	*get_next_line(int fd)
 		buffer = NULL;
 		return (NULL);
 	}
-	printf("buffber%s", buffer);
+	// // printf("buff before ft_line: %s \n", buffer);
 	line = ft_line(buffer);
+	// printf("buffer after ft_line: %s", buffer);	
 	buffer = next_line(buffer);
-	printf("buff:%s", buffer);
+	//  printf("buff:%s", buffer);
 	return (line);
 }
